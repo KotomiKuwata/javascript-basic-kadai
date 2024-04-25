63c56ba75c8a282ce41cacfb086ee2e14e232e36
KotomiKuwata <163006489+KotomiKuwata@users.noreply.github.com>
2024-04-25 23:39:35 +0900

    kadai_014を修正

--- kadai_014/sqare.js
+++ kadai_014/sqare.js
@@ -1,5 +1,10 @@
-const sqared = (num) => {
+/*const sqared = (num) => {
     console.log (num ** 2);
 }
+sqared(10);*/
 
-sqared(10);
+const sqareNumber = Math.floor (Math.random () * 6);
+const sqare = (num) => {
+    return num ** 2;
+}
+console.log (sqare (sqareNumber));
